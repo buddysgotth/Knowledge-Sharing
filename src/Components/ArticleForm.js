8238cf8470605914fe9e7a924df3b39e635bd989
Yootthatapong Jannusith <59070144@it.kmitl.ac.th>
2019-10-31 19:07:00 +0700

    [ADD] insert 'disabledForm' func. in 'ArticleForm'

--- src/Components/ArticleForm.js
+++ src/Components/ArticleForm.js
@@ -2,16 +2,9 @@ import React from "react";
 import axios from "axios";
 import log from "loglevel";
 import PropTypes from "prop-types";
-import {
-  Container,
-  Form,
-  Columns,
-  Tile,
-  Heading
-} from "react-bulma-components";
+import { Form, Columns, Tile, Heading } from "react-bulma-components";
 import ReactQuill from "react-quill";
 
-import Loading from "./Loading";
 import TitleForm from "./TitleForm";
 import CategoryForm, { CategoriesPropTypes } from "./CategoryForm";
 import TagsInput, { TagsPropTypes } from "./TagsInput";
@@ -52,8 +45,7 @@ class ArticleForm extends React.Component {
       category: "",
       tags: null,
       content: "",
-      isCreated: "none",
-      isLoading: false
+      status: "none"
     };
     this.handleTagsChange = this.handleTagsChange.bind(this);
     this.handleOnChange = this.handleOnChange.bind(this);
@@ -61,15 +53,21 @@ class ArticleForm extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  showArticleCreatedStatus = isCreated => {
-    if (isCreated === "publish") {
+  disabledForm = status => {
+    if (status === "creating") {
+      return true;
+    }
+    return false;
+  };
+  showArticleCreatedStatus = status => {
+    if (status === "publish") {
       return (
         <Tile kind="parent" notification color="success">
           <Heading subtitle>Create Article Complete</Heading>
         </Tile>
       );
     }
-    if (isCreated === "draft") {
+    if (status === "draft") {
       return (
         <Tile kind="parent" notification color="info">
           <Heading subtitle>Draft Article Complete</Heading>
@@ -79,19 +77,19 @@ class ArticleForm extends React.Component {
     return null;
   };
   handleTagsChange = tags => {
-    this.setState({ isCreated: "none", tags: tags });
+    this.setState({ status: "none", tags: tags });
   };
   handleOnChange = e => {
-    this.setState({ isCreated: "none", [e.target.name]: e.target.value });
+    this.setState({ status: "none", [e.target.name]: e.target.value });
   };
   textEditorOnChange = value => {
-    this.setState({ isCreated: "none", content: value });
+    this.setState({ status: "none", content: value });
   };
   handleSubmit = async e => {
     log.debug(e.target.value);
     const { title, category, tags, content } = this.state;
     const status = e.target.value;
-    this.setState({ isLoading: true });
+    this.setState({ status: "creating" });
     await axios
       .post(
         "/wp-json/wp/v2/articles",
@@ -113,53 +111,53 @@ class ArticleForm extends React.Component {
       )
       .then(res => {
         log.debug(res);
-        this.setState({ isLoading: false, isCreated: status });
+        this.setState({ status: status });
       })
       .catch(error => log.error());
   };
 
   render() {
-    const { title, category, content, tags, isCreated, isLoading } = this.state;
+    const { title, category, content, tags, status } = this.state;
     const { categories } = this.props;
-    if (isLoading) {
-      return <Loading />;
-    }
     return (
-      <Container>
-        {this.showArticleCreatedStatus(isCreated)}
-        <TitleForm onChange={this.handleOnChange} value={title} />
-        <Columns>
-          <Columns.Column>
-            <CategoryForm
-              onChange={this.handleOnChange}
-              value={category}
-              categories={categories}
+      <React.Fragment>
+        {this.showArticleCreatedStatus(status)}
+        <fieldset disabled={this.disabledForm(status)}>
+          <TitleForm onChange={this.handleOnChange} value={title} />
+          <Columns>
+            <Columns.Column>
+              <CategoryForm
+                onChange={this.handleOnChange}
+                value={category}
+                categories={categories}
+              />
+            </Columns.Column>
+            <Columns.Column>
+              <TagsInput
+                token={this.props.token}
+                value={tags}
+                options={this.props.tags}
+                onChange={this.handleTagsChange}
+              />
+            </Columns.Column>
+          </Columns>
+          <Form.Field>
+            <Form.Label>Description</Form.Label>
+            <ReactQuill
+              value={content}
+              onChange={this.textEditorOnChange}
+              modules={modules}
+              formats={formats}
+              placeholder="What do you want to share..."
+              readOnly={this.disabledForm(status)}
             />
-          </Columns.Column>
-          <Columns.Column>
-            <TagsInput
-              token={this.props.token}
-              value={tags}
-              options={this.props.tags}
-              onChange={this.handleTagsChange}
-            />
-          </Columns.Column>
-        </Columns>
-        <Form.Field>
-          <Form.Label>Description</Form.Label>
-          <ReactQuill
-            value={content}
-            onChange={this.textEditorOnChange}
-            modules={modules}
-            formats={formats}
-            placeholder="What do you want to share..."
+          </Form.Field>
+          <CreateButtonForm
+            onSubmit={this.handleSubmit}
+            data={{ title, category, tags, content }}
           />
-        </Form.Field>
-        <CreateButtonForm
-          onSubmit={this.handleSubmit}
-          data={{ title, category, tags, content }}
-        />
-      </Container>
+        </fieldset>
+      </React.Fragment>
     );
   }
 }
